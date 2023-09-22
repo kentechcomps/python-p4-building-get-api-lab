3844a8717972697137eb02aa88cebbcf43c5ccbd
user <mutukukennedy34@gmail.com>
2023-09-22 08:44:55 +0300

    getapi

--- server/app.py
+++ server/app.py
@@ -20,19 +20,39 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
+    bakeries = Bakery.query.all()
+    serialized_bakeries = [bakery.to_dict() for bakery in bakeries]
+    
+    response = make_response(
+        jsonify(serialized_bakeries),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
     return ''
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get(id)
+    if bakery:
+        serialized_bakery = bakery.to_dict(nested=['baked_goods'])
+        return jsonify(serialized_bakery)
+    else:
+        return jsonify({"message": "Bakery not found"}), 404
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    serialized_baked_goods = [good.to_dict() for good in baked_goods]
+    return jsonify(serialized_baked_goods)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if baked_good:
+        return jsonify(baked_good.to_dict())
+    else:
+        return jsonify({"message": "No baked goods found"}), 404
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -12,9 +12,21 @@ class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    # Define a one-to-many relationship with BakedGood
+    baked_goods = db.relationship('BakedGood', backref='bakery', lazy=True)
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
     id = db.Column(db.Integer, primary_key=True)
-    
+    name = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    # Define a foreign key to link BakedGood to Bakery
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'), nullable=False)
